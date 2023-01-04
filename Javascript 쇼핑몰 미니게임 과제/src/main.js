383723074962428c05335c871f1198ed680056ee
hyjun2352 <khyj030502@gmail.com>
2023-01-04 22:10:40 +0900

    JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -5,11 +5,27 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  const html = items.map((item) => createHTMLString(item)).join("");
+  console.log(html);
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+function createHTMLString(item) {
+  return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
 // main
 loadItems()
   .then((items) => {
-    console.log(item);
-    // displayItems(items);
+    console.log(items);
+    displayItems(items);
     // setEventListensers(items);
   })
   .catch(console.log);
