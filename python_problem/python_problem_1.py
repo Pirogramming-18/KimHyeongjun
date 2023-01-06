c1e477bedaa416d1fd331cb80fdc9483739c4fc1
hyjun2352 <khyj030502@gmail.com>
2023-01-07 02:26:24 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,15 +1,28 @@
 num = 0
+num_sum = 0
+name = "player"
+a = True
 
-while True:
-    number = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
-    if not number.isnumeric():
-        print('정수를 입력하세요')
-    elif number == "1" or number == "2" or number == "3":
-        break
+for i in range(2):
+    while True:
+        number = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+        if not number.isnumeric():
+            print('정수를 입력하세요')
+        elif number == "1" or number == "2" or number == "3":
+            break
+        else:
+            print('1,2,3 중 하나를 입력하세요')
+
+    number = int(number)
+    num_sum += number
+    if a == True:
+        name = "playerA"
+        a = False
     else:
-        print('1,2,3 중 하나를 입력하세요')
+        name = "playerB"
+        a = True
 
-number = int(number)
-while num < number:
-    num+=1
-    print(num)
+    while num < num_sum:
+        num+=1
+        print(name, ":",  num)
+    
