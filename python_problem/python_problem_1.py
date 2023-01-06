e8e336278d540f75f0fedf8a7376b11e70b415dd
hyjun2352 <khyj030502@gmail.com>
2023-01-07 02:10:05 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,10 @@
 num = 0
 
-number = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+while True:
+    number = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
+    if not number.isnumeric():
+        print('정수를 입력하세요')
+    elif number == "1" or number == "2" or number == "3":
+        break
+    else:
+        print('1,2,3 중 하나를 입력하세요')
