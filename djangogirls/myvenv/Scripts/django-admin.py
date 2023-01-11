ff2830565986448052ca1b68ed1177d4528d093d
hyjun2352 <khyj030502@gmail.com>
2023-01-12 05:41:54 +0900

    Django 설치하기

---
+++ djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\khyj0\OneDrive\바탕 화면\Developer\Kimhyeongjun\djangogirls\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
